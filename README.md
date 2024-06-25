51b55dba35e3c9768c6c3d5c21c356ad724a14f8
Raisson A. Santana <almeidaraisson@gmail.com>
2024-06-24 21:06:15 -0300

    update

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center"> KoviskDecoder - Codificador e Decodificador de texto. </h1>
+
+Decodificador de texto simples para uso recreativo.
+
+
+<img src="[https://i.imgur.com/gp1e2mf.png](https://github.com/raikovisk/KoviskDecoder/assets/58261944/ac904667-291d-4116-9300-a0cb3f3cee73)" width="200" height="200" />
+
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=FINALIZADO&color=GREEN&style=for-the-badge)
+
+# 📑 Resumo do desafio
+Desenvolver um uma aplicação que criptografa textos, assim você poderá trocar mensagens secretas com outras pessoas que saibam o segredo da criptografia utilizada.
+
+Este desafio de programação faz parte do projeto Oracle Next One T6.
+
+
+## 🔨 Funcionalidades do projeto
+
+- As "chaves" de criptografia que utilizaremos são:
+- A letra "e" é convertida para "enter"
+- A letra "i" é convertida para "imes"
+- A letra "a" é convertida para "ai"
+- A letra "o" é convertida para "ober"
+- A letra "u" é convertida para "ufat"
+
+
+## ✔️ Tecnologias utilizadas
+
+- ``HTML``
+- ``CSS``
+- ``Javascript``
+- ``VSCode``
+- ``Conceitos de Docment Object Model``
+- ``Conceitos de Transition e Animation do CSS``
+
+
+## 👨‍💻 Executar o projeto
+
+```bash
+******
+```
+
+## Licença
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+const btnEncript = document.getElementById('btnEncript')
+const btnDecript = document.getElementById('btnEncript')
+const btnCopy = document.getElementById('btnCopy')
+
+btnEncrypt.onclick = () => {
+    const textEntry = document.getElementById('textEntry').value
+    const areaOutput = document.getElementById('outputArea')
+    const outputText = document.getElementById('outputText')
+    const hideArea = document.getElementById('hideArea')
+    const hideArea2 = document.getElementById('hideArea2')
+    const arrayText = textEntry.split('')
+    const arrayListEncrypt = []
+
+    arrayText.forEach(element => {
+        if(element === 'a'){
+            arrayListEncrypt.push('ai')
+        } else if (element === 'e'){
+            arrayListEncrypt.push('enter')
+        } else if (element === 'i'){
+            arrayListEncrypt.push('imes')
+        } else if (element === 'o'){
+            arrayListEncrypt.push('ober')
+        } else if (element === 'u'){
+            arrayListEncrypt.push('ufat')
+        } else {
+            arrayListEncrypt.push(element)
+        }
+    });
+    
+    hideArea.style.display = 'none'
+    hideArea2.style.display = 'none'
+    areaOutput.style.justifyContent = 'start'
+    outputText.innerHTML = arrayListEncrypt.join('')
+
+    btnCopy.style.display = '';
+    
+}
+
+btnDecrypt.onclick = () => {
+    const textEntry = document.getElementById('textEntry').value
+    const areaOutput = document.getElementById('outputArea')
+    const outputText = document.getElementById('outputText')
+    const hideArea = document.getElementById('hideArea')
+    const hideArea2 = document.getElementById('hideArea2')
+    const arrayListEncrypt = []
+
+    const decrypt = textEntry.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u')
+    
+    hideArea.style.display = 'none'
+    hideArea2.style.display = 'none'
+    areaOutput.style.justifyContent = 'start'
+    outputText.innerHTML = arrayListEncrypt.join('')
+    btnCopy.style.display = 'none';
+}
+
+btnCopy.onclick = () => {
+    const outputText = document.getElementById('outputText')
+    btnCopy.style.display = 'none';
+    navigator.clipboard.writeText(outputText.textContent)
+    .then(() => {
+        alert('Texto copiado con éxito')
+        })
+        .catch(() => {
+            alert('Error al copiar texto')
+            })      
+}
